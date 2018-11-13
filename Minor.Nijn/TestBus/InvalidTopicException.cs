bf32f2198189d62b9cf3598daf1fdfa0af93a4b7
Dylan van Gils <dylan.vangils@student.hu.nl>
2018-11-13 15:00:28 +0100

    Added TopicInvalidException

---
+++ Minor.Nijn/TestBus/InvalidTopicException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Minor.Nijn.TestBus
+{
+    [Serializable]
+    internal class InvalidTopicException : Exception
+    {
+        public InvalidTopicException()
+        {
+        }
+
+        public InvalidTopicException(string message) : base(message)
+        {
+        }
+
+        public InvalidTopicException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidTopicException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
