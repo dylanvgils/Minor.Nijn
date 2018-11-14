7d44d98841d85b0d52881cd8dbf98db2629bdaad
Dion de Koning <dion.dekoning@student.hu.nl>
2018-11-14 12:31:53 +0100

    Added custom event entity and configured RabbitMq in startup

--- AuditlogMicroservice/Entities/Event.cs
+++ AuditlogMicroservice/Entities/Event.cs
@@ -5,13 +5,11 @@ using System.Threading.Tasks;
 
 namespace AuditlogMicroservice.Entities
 {
-    public class EventMessage
+    public class Event
     {
         public long Id { get; set; }
         public string RoutingKey { get; set; }
-        public string Message { get; set; }
-        public string EventType { get; set; }
         public long Timestamp { get; set; }
-        public string CorrelationId { get; set; }
+        public string Message { get; set; }
     }
 }
