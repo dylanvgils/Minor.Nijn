c0e7629cf0f04a25f583faff678e12e30062de87
Dylan van Gils <9094916+dylanvgils@users.noreply.github.com>
2018-12-04 09:43:26 +0100

    Added check to return type of command listener methods

---
+++ Minor.Nijn.WebScale.Test/InvalidTestClasses/InvalidCommandListenerException.cs
@@ -0,0 +1,16 @@
+﻿using Minor.Nijn.WebScale.Attributes;
+using Minor.Nijn.WebScale.Test.TestClasses.Commands;
+using System;
+
+namespace Minor.Nijn.WebScale.Test.InvalidTestClasses
+{
+    [CommandListener]
+    class InvalidCommandListenerException
+    {
+        [Command("InvalidCommandQueue")]
+        public int ThrowException(AddProductCommand request)
+        {
+            throw new NullReferenceException("Some null reference exception");
+        }
+    }
+}
