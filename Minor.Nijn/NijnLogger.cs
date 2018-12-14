09bb50e112deecccb7ebfbc1e7f04716dca22d45
Dylan van Gils <9094916+dylanvgils@users.noreply.github.com>
2018-12-14 13:49:14 +0100

    Improved loggers and updated dependencies

--- Minor.Nijn/NijnLogger.cs
+++ Minor.Nijn/NijnLogger.cs
@@ -1,19 +1,26 @@
 ﻿using Microsoft.Extensions.Logging;
+using Serilog;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Minor.Nijn
 {
     internal static class NijnLogger
     {
-        public static ILoggerFactory DefaultFactory { get; } = new LoggerFactory();
         public static ILoggerFactory LoggerFactory { get; set; }
+
+        static NijnLogger()
+        {
+            LoggerFactory = new LoggerFactory()
+                .AddSerilog(new LoggerConfiguration()
+                    .MinimumLevel.Debug()
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console()
+                    .CreateLogger()
+                );
+        }
 
         public static ILogger CreateLogger<T>()
         {
-            if (LoggerFactory == null)
-            {
-                return DefaultFactory.CreateLogger<T>();
-            }
-
             return LoggerFactory.CreateLogger<T>();
         }
     }
