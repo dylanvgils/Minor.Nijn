3b6914ee718bb8045b8a69304dece496b4984141
Dylan van Gils <dylanvangils@gmail.com>
2019-01-13 11:43:39 +0100

    Add ReadOnlyCollectionHelper and add docs to IMicroServiceHost

---
+++ Minor.Nijn.WebScale.Test/Helpers/ReadOnlyCollectionExtensionsTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Minor.Nijn.WebScale.Helpers;
+
+namespace Minor.Nijn.WebScale.Test.Helpers
+{
+    [TestClass]
+    public class ReadOnlyCollectionExtensionsTest
+    {
+        [TestMethod]
+        public void ForEach_ShouldLoopOverList()
+        {
+            List<string> strings = new List<string>();
+            strings.Add("one");
+            strings.Add("two");
+
+            var result = new List<string>();
+            IReadOnlyList<string> target = strings.AsReadOnly();
+            target.ForEach(s => result.Add(s));
+
+            Assert.AreEqual(strings[0], result[0]);
+            Assert.AreEqual(strings[1], result[1]);
+        }
+    }
+}
---
+++ Minor.Nijn.WebScale/Helpers/ReadOnlyCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Minor.Nijn.WebScale.Helpers
+{
+    public static class ReadOnlyCollectionExtensions
+    {
+        public static void ForEach<T>(this IReadOnlyCollection<T> items, Action<T> action)
+        {
+            foreach (var item in items)
+            {
+                action(item);
+            }
+        }    
+    }
+}
