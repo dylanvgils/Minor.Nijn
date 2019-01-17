bc039862f28b24fb2ef803d377f7d1c25d4132dc
Dylan van Gils <9094916+dylanvgils@users.noreply.github.com>
2019-01-17 17:55:21 +0100

    Add Indexer to TestBus queues

--- Minor.Nijn.Test/TestBus/EventBus/EventBusQueueTest.cs
+++ Minor.Nijn.Test/TestBus/EventBus/EventBusQueueTest.cs
@@ -7,22 +7,22 @@ namespace Minor.Nijn.TestBus.EventBus.Test
     [TestClass]
     public class EventBusQueueTest
     {
-        private EventBusQueue target;
+        private EventBusQueue _target;
 
         [TestInitialize]
         public void BeforeEach()
         {
             string queueName = "TestQueue";
             IEnumerable<string> topicExpressions = new List<string> { "a.b.c" };
-            target = new EventBusQueue(queueName, topicExpressions);
+            _target = new EventBusQueue(queueName, topicExpressions);
         }
 
         [TestMethod]
         public void Subscribe_ShouldSubscribeToTheMessageAddedEvent()
         {
             var mock = new MessageAddedMock<EventMessage>();
-            target.Subscribe(mock.HandleMessageAdded);
-            Assert.AreEqual(1, target.SubscriberCount);
+            _target.Subscribe(mock.HandleMessageAdded);
+            Assert.AreEqual(1, _target.SubscriberCount);
         }
 
         [TestMethod]
@@ -31,10 +31,10 @@ namespace Minor.Nijn.TestBus.EventBus.Test
             var mock1 = new MessageAddedMock<EventMessage>();
             var mock2 = new MessageAddedMock<EventMessage>();
 
-            target.Subscribe(mock1.HandleMessageAdded);
-            target.Subscribe(mock2.HandleMessageAdded);
+            _target.Subscribe(mock1.HandleMessageAdded);
+            _target.Subscribe(mock2.HandleMessageAdded);
 
-            Assert.AreEqual(2, target.SubscriberCount);
+            Assert.AreEqual(2, _target.SubscriberCount);
         }
 
         [TestMethod]
@@ -42,12 +42,12 @@ namespace Minor.Nijn.TestBus.EventBus.Test
         {
             var mock1 = new MessageAddedMock<EventMessage>();
             var mock2 = new MessageAddedMock<EventMessage>();
-            target.Subscribe(mock1.HandleMessageAdded);
-            target.Subscribe(mock2.HandleMessageAdded);
+            _target.Subscribe(mock1.HandleMessageAdded);
+            _target.Subscribe(mock2.HandleMessageAdded);
 
-            target.Unsubscribe(mock1.HandleMessageAdded);
+            _target.Unsubscribe(mock1.HandleMessageAdded);
 
-            Assert.AreEqual(1, target.SubscriberCount);
+            Assert.AreEqual(1, _target.SubscriberCount);
         }
 
         [TestMethod]
@@ -56,10 +56,10 @@ namespace Minor.Nijn.TestBus.EventBus.Test
             var message1 = new EventMessage("a.b.c", "Test message 1");
             var message2 = new EventMessage("a.b.c", "Test message 2");
 
-            target.Enqueue(message1);
-            target.Enqueue(message2);
+            _target.Enqueue(message1);
+            _target.Enqueue(message2);
 
-            Assert.AreEqual(2, target.MessageQueueLength);
+            Assert.AreEqual(2, _target.MessageQueueLength);
         }
 
         [TestMethod]
@@ -67,15 +67,15 @@ namespace Minor.Nijn.TestBus.EventBus.Test
         {
             var message1 = new EventMessage("a.b.c", "Test message 1");
             var message2 = new EventMessage("a.b.c", "Test message 2");
-            target.Enqueue(message1);
-            target.Enqueue(message2);
+            _target.Enqueue(message1);
+            _target.Enqueue(message2);
 
             var mock = new MessageAddedMock<EventMessage>();
-            target.Subscribe(mock.HandleMessageAdded);
+            _target.Subscribe(mock.HandleMessageAdded);
 
             Assert.IsTrue(mock.HandledMessageAddedHasBeenCalled);
             Assert.AreEqual(2, mock.HandleMessageAddedCount);
-            Assert.AreEqual(0, target.MessageQueueLength);
+            Assert.AreEqual(0, _target.MessageQueueLength);
         }
 
         [TestMethod]
@@ -83,9 +83,9 @@ namespace Minor.Nijn.TestBus.EventBus.Test
         {
             var mock = new MessageAddedMock<EventMessage>();
             var message = new EventMessage("a.b.c", "Test message.");
-            target.Subscribe(mock.HandleMessageAdded);
+            _target.Subscribe(mock.HandleMessageAdded);
 
-            target.Enqueue(message);
+            _target.Enqueue(message);
 
             Assert.IsTrue(mock.HandledMessageAddedHasBeenCalled);
             Assert.AreEqual(message, mock.Args.Message);
@@ -96,11 +96,27 @@ namespace Minor.Nijn.TestBus.EventBus.Test
         {
             var mock = new MessageAddedMock<EventMessage>();
             var message = new EventMessage("a.b.d", "Test message.");
-            target.Subscribe(mock.HandleMessageAdded);
+            _target.Subscribe(mock.HandleMessageAdded);
 
-            target.Enqueue(message);
+            _target.Enqueue(message);
 
             Assert.IsFalse(mock.HandledMessageAddedHasBeenCalled);
+        }
+
+        [TestMethod]
+        public void Indexer_ShouldReturnItemAtIndex()
+        {
+            var msg1 = new EventMessage("a.b.c", "Message1");
+            var msg2 = new EventMessage("a.b.c", "Message2");
+            var msg3 = new EventMessage("a.b.c", "Message3");
+   
+            _target.Enqueue(msg1);
+            _target.Enqueue(msg2);
+            _target.Enqueue(msg3);
+
+            Assert.AreEqual(msg1, _target[0]);
+            Assert.AreEqual(msg2, _target[1]);
+            Assert.AreEqual(msg3, _target[2]);
         }
     }
 }
